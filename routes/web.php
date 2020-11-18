d7acff5b030139b46d7aeb755b0db746364b3e37
nakano-ss <25.shobu.25@gmail.com>
2020-11-18 20:45:08 +0900

    first09 commit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/edit', 'Admin\ProfileController@add');
+});
